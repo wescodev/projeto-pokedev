62b78bf64391802bffdb00819a0e9cd6e22c2f79
Wesley <96826482+wescodev@users.noreply.github.com>
2024-10-19 23:45:10 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,36 @@
+const listaSelecaoPokedevs = document.querySelectorAll(".pokedev");
+
+listaSelecaoPokedevs.forEach(pokedev => {
+    pokedev.addEventListener("click", () => {
+        RemoverCartaoPokedev();
+
+        const idPokedevSelecionado = MostrarCartaoPokedevSelecionado(pokedev);
+
+        DesativarPokedevNaListagem();
+
+        AtivarPokedevSelecionadoNaListagem(idPokedevSelecionado);
+    })
+})
+
+function AtivarPokedevSelecionadoNaListagem(idPokedevSelecionado) {
+    const pokedevSelecionadoNaListagem = document.getElementById(idPokedevSelecionado);
+    pokedevSelecionadoNaListagem.classList.add("ativo");
+}
+
+function DesativarPokedevNaListagem() {
+    const pokedevAtivoNaListagem = document.querySelector(".ativo");
+    pokedevAtivoNaListagem.classList.remove("ativo");
+}
+
+function MostrarCartaoPokedevSelecionado(pokedev) {
+    const idPokedevSelecionado = pokedev.attributes.id.value;
+    const idDoCartaoPokedevParaAbrir = "cartao-" + idPokedevSelecionado;
+    const cartaoPokedevParaAbrir = document.getElementById(idDoCartaoPokedevParaAbrir);
+    cartaoPokedevParaAbrir.classList.add("aberto");
+    return idPokedevSelecionado;
+}
+
+function RemoverCartaoPokedev() {
+    const cartaoPokedevAberto = document.querySelector(".aberto");
+    cartaoPokedevAberto.classList.remove("aberto");
+}
